94b8deb29d5e52ab99c6476c81ebc4055d3d0240
TienDoMinh <tiendm.15@grad.uit.edu.vn>
2026-04-26 21:36:05 +0700

    feat

--- src/components/RelatedLink/RelatedLink.jsx
+++ src/components/RelatedLink/RelatedLink.jsx
@@ -5,7 +5,7 @@ const RelatedLink = () => {
         <div className='related-links container-size mb-12'>
             <h2 className='text-2xl text-black font-bold'>Related Links</h2>
             <p className='mt-8'>
-                If you would like to utilize RGB images, please fill the MM-Fi Dataset Request Form.
+                If you would like to utilize RGB images, the WiVi32 Dataset Request From: https://drive.google.com/drive/u/3/folders/1s4TtS8UjizoMxQY0jQDFPiDxcqK4Hw1C.
 
             </p>
         </div>
